aaba5ba9efe2f57978ed02f3bc23d46fd64ec84d
Sparkle Russell-Puleri <sparalic@gmail.com>
2019-03-22 11:39:58 -0700

    updated

--- gists/encoding.py
+++
@@ -1,5 +0,0 @@
-character_list = list(set(text))   # get all of the unique letters in our text variable
-vocabulary_size = len(character_list)   # count the number of unique elements
-character_dictionary = {'h': 0, 'a': 1, 't': 2, 'M': 3}
-# {char:e for e, char in enumerate(character_list)}  # create a dictionary mapping each unique char to a number
-encoded_chars = [character_dictionary[char] for char in text] #integer representation of our vocabulary 
